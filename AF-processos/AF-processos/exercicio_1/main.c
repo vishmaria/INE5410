8db04d713a44962d0eb97796836fc0bb3dac5f98
vishmaria <maria.fernandabt99@gmail.com>
2022-05-06 11:17:31 -0300

    Exercicio 1

--- AF-processos/AF-processos/exercicio_1/main.c
+++ AF-processos/AF-processos/exercicio_1/main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
 
 
 //       (pai)      
@@ -27,7 +32,35 @@ int main(int argc, char** argv) {
      * 2. Faça os prints exatamente como solicitado. *
      * 3. Espere o término dos filhos                *
      *************************************************/
+    pid_t filho1, filho2;
+   /* if (filho1 >= 0){
+        if (filho1 ==0){
+            printf("Processo filho %d criado\n", getpid());
+            wait(NULL);
+        }
+        else {
+            printf("Processo pai criou %d\n", filho1);
+            filho2 = fork();
+            wait(NULL);
+        }
+    }*/
 
-    printf("Processo pai finalizado!\n");   
+    for (int i = 0; i < 2; i++) {
+        filho1 = fork();
+        if (filho1 >= 0){
+            if (filho1 == 0){
+                printf("Processo filho %d criado\n", getpid());
+                break;
+            }
+            else{
+               printf("Processo pai criou %d\n", filho1);
+               wait(NULL);
+               if (i == 1) printf("Processo pai finalizado!\n");   
+            }
+        }
+    }
+    
+
+     
     return 0;
 }
