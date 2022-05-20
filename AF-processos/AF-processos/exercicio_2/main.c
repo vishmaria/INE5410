4b45dea5186e4cbfeaeb8d62b7d7b4ef05c19eb4
vishmaria <maria.fernandabt99@gmail.com>
2022-05-20 10:21:24 -0300

    ataulização pasta correta

--- AF-processos/AF-processos/exercicio_2/main.c
+++ AF-processos/AF-processos/exercicio_2/main.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
 
 //                          (principal)
 //                               |
@@ -31,6 +34,36 @@ int main(int argc, char** argv) {
      * 3. Espere o término dos filhos                *
      *************************************************/
 
-    printf("Processo principal %d finalizado\n", getpid());    
+    pid_t filho1, filho2;
+    int n = 2;
+    int m = 3;
+    for (int i = 0; i < n; i++) {
+        filho1 = fork();
+        if (filho1 >= 0){
+            if (filho1 == 0){
+                printf("Processo %d, filho de %d\n", getpid(), getppid());
+                for (int c = 0; c < m; c++) {
+                    filho2 = fork();
+                    if (filho2 >= 0){
+                        if (filho2 ==0){
+                            printf("Processo %d, filho de %d\n", getpid(), getppid());
+                            break;
+                        }
+                        else{
+                            sleep(5);
+                            printf("Processo %d finalizado\n", filho2);
+                        }
+                    }
+                }
+                break;
+            }
+            else{
+               wait(NULL);
+               printf("Processo %d finalizado\n", filho1); 
+               if (i == n-1) printf("Processo principal %d finalizado!\n", getpid());   
+            }
+        }
+    }
+
     return 0;
 }
---
+++ AF-threads/exercicio_2/main.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <pthread.h>
+
+// Lê o conteúdo do arquivo filename e retorna um vetor E o tamanho dele
+// Se filename for da forma "gen:%d", gera um vetor aleatório com %d elementos
+//
+// +-------> retorno da função, ponteiro para vetor malloc()ado e preenchido
+// | 
+// |         tamanho do vetor (usado <-----+
+// |         como 2o retorno)              |
+// v                                       v
+double* load_vector(const char* filename, int* out_size);
+
+
+// Avalia o resultado no vetor c. Assume-se que todos os ponteiros (a, b, e c)
+// tenham tamanho size.
+void avaliar(double* a, double* b, double* c, int size);
+
+
+int main(int argc, char* argv[]) {
+    // Gera um resultado diferente a cada execução do programa
+    // Se **para fins de teste** quiser gerar sempre o mesmo valor
+    // descomente o srand(0)
+    srand(time(NULL)); //valores diferentes
+    //srand(0);        //sempre mesmo valor
+
+    //Temos argumentos suficientes?
+    if(argc < 4) {
+        printf("Uso: %s n_threads a_file b_file\n"
+               "    n_threads    número de threads a serem usadas na computação\n"
+               "    *_file       caminho de arquivo ou uma expressão com a forma gen:N,\n"
+               "                 representando um vetor aleatório de tamanho N\n",
+               argv[0]);
+        return 1;
+    }
+  
+    //Quantas threads?
+    int n_threads = atoi(argv[1]);
+    if (!n_threads) {
+        printf("Número de threads deve ser > 0\n");
+        return 1;
+    }
+    //Lê números de arquivos para vetores alocados com malloc
+    int a_size = 0, b_size = 0;
+    double* a = load_vector(argv[2], &a_size);
+    if (!a) {
+        //load_vector não conseguiu abrir o arquivo
+        printf("Erro ao ler arquivo %s\n", argv[2]);
+        return 1;
+    }
+    double* b = load_vector(argv[3], &b_size);
+    if (!b) {
+        printf("Erro ao ler arquivo %s\n", argv[3]);
+        return 1;
+    }
+    
+    //Garante que entradas são compatíveis
+    if (a_size != b_size) {
+        printf("Vetores a e b tem tamanhos diferentes! (%d != %d)\n", a_size, b_size);
+        return 1;
+    }
+    //Cria vetor do resultado 
+    double* c = malloc(a_size*sizeof(double));
+
+    // Calcula com uma thread só. Programador original só deixou a leitura 
+    // do argumento e fugiu pro caribe. É essa computação que você precisa 
+    // paralelizar
+    for (int i = 0; i < a_size; ++i) 
+        c[i] = a[i] + b[i];
+
+    //    +---------------------------------+
+    // ** | IMPORTANTE: avalia o resultado! | **
+    //    +---------------------------------+
+    avaliar(a, b, c, a_size);
+    
+
+    //Importante: libera memória
+    free(a);
+    free(b);
+    free(c);
+
+    return 0;
+}
