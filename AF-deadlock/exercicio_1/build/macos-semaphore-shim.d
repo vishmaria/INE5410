e180c578d41f0b2f69deb1ce1a8133ad55896684
vishmaria <54604250+vishmaria@users.noreply.github.com>
2022-06-03 11:37:02 -0300

    Exercício deadlocks 03/06

---
+++ AF-deadlock/exercicio_1/build/macos-semaphore-shim.d
@@ -0,0 +1,6 @@
+build/macos-semaphore-shim.o: macos-semaphore-shim.c semaphore.h \
+ /usr/include/semaphore.h
+
+semaphore.h:
+
+/usr/include/semaphore.h:
---
+++ AF-deadlock/exercicio_1/build/main.d
@@ -0,0 +1,5 @@
+build/main.o: main.c semaphore.h /usr/include/semaphore.h
+
+semaphore.h:
+
+/usr/include/semaphore.h:
